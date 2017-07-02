3dcf1f85e22703a804ecdc86113c710445dcf5df
437399 <jeetendra.login@gmail.com>
2017-07-02 19:51:29 +0530

    my testing

---
+++ src/test/java/Hello02/testing01.java
@@ -0,0 +1,14 @@
+package Hello02;
+import org.testng.annotations.*;
+public class testing01 {
+	
+	@Test
+	public void launch(){
+		System.out.println("The value of Lavana");
+		System.out.println("The value of Dhruvi");
+		System.out.println("The value of Dhruvi");
+		System.out.println("The value of Dhruvi");
+		
+		
+	}
+}
